6f97366e0c6f70e146882162b2e1264a9c454826
Randy Sim <randysim9@gmail.com>
2024-08-18 19:43:17 -0400

    feat: SWR authentication

---
+++ frontend/src/app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client'
+
+export default function Dashboard() {
+    
+    
+    return (
+        <div>
+
+        </div>
+    )
+}
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,24 +1,8 @@
 "use client"
-import { useEffect, useState } from "react";
+import useUser from "@/app/lib/useUser";
 
 export default function Home() {
-  let [authUser, setAuthUser] = useState("");
-
-  useEffect(() => {
-    const getAuthenticatedUser = async () => {
-      try {
-        let res = await fetch("http://localhost:8080/api/v1/user/authenticated", { method: "GET", credentials: "include"});
-        console.log(res)
-        let json = await res.json();
-        setAuthUser(JSON.stringify(json));
-      } catch (error) {
-        console.log(error)
-        setAuthUser("No authenticated user");
-      }
-    }
-    
-    getAuthenticatedUser();
-  }, [])
+  let user = useUser();
 
   return (
     <div>
@@ -26,7 +10,7 @@ export default function Home() {
         With Google: <a href="http://localhost:8080/oauth2/authorization/google">click here</a>
       </div>
       <div>
-        {authUser}
+        {JSON.stringify(user)}
       </div>
     </div>
   );
